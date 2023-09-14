5ec824252896c930ec3cf2a5d162602b50773d7c
Omkar <oj6875770@gmail.com>
2023-09-14 20:24:14 +0530

    backend

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -29,7 +29,11 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://dalle-arbb.onrender.com/api/v1/dalle', {
+        const response1 = await fetch('http://localhost:8080/api/v1/dalle');
+        const data1 = await response1.json();
+        console.log('Response:', data1);
+
+        const response = await fetch('http://localhost:8080/api/v1/dalle', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
